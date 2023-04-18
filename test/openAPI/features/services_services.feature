5a0fa912dfeb02313e12ed5fdd15cd567f10efb9
Karolina Kopacz <karolina.kopacz94@gmail.com>
2023-04-18 13:53:36 +0200

    TECH-558: Changed scenarios title, added After

--- test/openAPI/features/services_services.feature
+++ test/openAPI/features/services_services.feature
@@ -7,21 +7,22 @@ Feature: API endpoint that returns a list of services (Services available to use
   Scenario: Retrieve the list of services smoke type test
 
     Given User wants to retrieve the list of services
-    When User sends GET request with given "Register Mother and Child" as name
+    When User sends GET /statistics request with no optional parameter
     Then The response from the /statistics endpoint is received
     And The /statistics response should be returned in a timely manner 15000ms
     And The /statistics response should have status 200
-    And The /statistics response should match json schema
+    And The /statistics response should match empty array
 
   @unit @positive
-  Scenario Outline: Retrieve the list of services
+  Scenario Outline: Retrieve the list of services that match the name provided in optional parameter
 
     Given User wants to retrieve the list of services
-    When User sends GET request with given "<name>" as name
+    When User sends GET /statistics request with given "<name>" as name
     Then The response from the /statistics endpoint is received
     And The /statistics response should be returned in a timely manner 15000ms
     And The /statistics response should have status 200
     And The /statistics response should match json schema
+    And The service name in response match provided "<name>" as name parameter
 
     Examples:
     | name                        |
--- test/openAPI/features/support/services_services.js
+++ test/openAPI/features/support/services_services.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const { spec } = require('pactum');
-const { Given, When, Then, Before } = require('@cucumber/cucumber');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
 const {
   servicesEndpoint,
   defaultExpectedResponseTime,
@@ -23,8 +23,8 @@ Given(
   () => 'User wants to retrieve the list of services'
 );
 
-When('User sends GET request with given {string} as name', name =>
-  specServices.withPath(baseUrl).withQueryParams('name', name)
+When('User sends GET \\/statistics request with no optional parameter', () =>
+  specServices.withPath(baseUrl)
 );
 
 Then(
@@ -44,11 +44,27 @@ Then('The \\/statistics response should have status 200', () =>
   specServices.response().to.have.status(200)
 );
 
+Then('The \\/statistics response should match empty array', () =>
+  chai.expect(specServices._response.json).to.be.jsonSchema([])
+);
+
+// Scenario Outline: Retrieve the list of services that match the name provided in optional parameter
+// Given and others Then for this scenario are written in the aforementioned example
+When('User sends GET \\/statistics request with given {string} as name', name =>
+  specServices.withPath(baseUrl).withQueryParams('name', name)
+);
+
 Then('The \\/statistics response should match json schema', () =>
   chai
     .expect(specServices._response.json)
     .to.be.jsonSchema(servicesResponseSchema)
 );
 
-// Scenario Outline: Retrieve the list of services
-// Given, When and others Then for this scenario are written in the aforementioned example
+Then(
+  'The service name in response match provided {string} as name parameter',
+  name => specServices.response().should.have.bodyContains('name', name)
+);
+
+After(endpointTag, () => {
+  specServices.end();
+});
